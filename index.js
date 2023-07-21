7ccc786d17bd9195045c15f0995eb71c63c1c9b0
Michael J. Shalewski <mshalewski@outlook.com>
2023-07-20 22:54:01 -0500

    added messages GET endpoint and changed contact endpoint notation

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const execute = require("child_process").execSync;
 const app = express();
 
-app.get("/contact-list", (req, res) => {
+app.get("/get-contacts", (req, res) => {
     res.send(`${execute("termux-contact-list")}`);
+});
+
+app.get("/get-messages", (req, res) => {
+    res.send(`${execute("termux-sms-list -d -n -t all")}`);
 });
 
 app.listen(8000);
