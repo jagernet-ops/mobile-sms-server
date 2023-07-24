d44f12d61ffc9615314410fb4a280eb1626e482b
Michael J. Shalewski <mshalewski@outlook.com>
2023-07-23 21:58:23 -0500

    use cors npm package

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const execute = require("child_process").execSync;
 const app = express();
+const cors = require("cors");
 
-const corsMiddleware = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, PUT, PATCH, POST, DELETE"
-    );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Content-Type, X-Requested-With, Authorization"
-    );
-    next();
-};
-
-app.use(corsMiddleware);
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.get("/get-contacts", (req, res) => {
     res.send(`${execute("termux-contact-list")}`);
