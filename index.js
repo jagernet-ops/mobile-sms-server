23757dafe3de447d87a36ef7fd437118fc63a5cd
Michael J. Shalewski <mshalewski@outlook.com>
2023-07-23 22:01:35 -0500

    reverted changes for sanity check

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const execute = require("child_process").execSync;
 const app = express();
-const cors = require("cors");
 
-app.use(cors({ origin: "http://localhost:5173" }));
+const corsMiddleware = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, PUT, PATCH, POST, DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type, X-Requested-With, Authorization"
+    );
+    next();
+};
+
+app.use(corsMiddleware);
 
 app.get("/get-contacts", (req, res) => {
     res.send(`${execute("termux-contact-list")}`);
