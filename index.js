bf59c10931958d9a13ed2e15c47c0251e14a3684
Michael J. Shalewski <mshalewski@outlook.com>
2023-07-23 21:12:26 -0500

    added cors to contacts endpoint

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const execute = require("child_process").execSync;
 const app = express();
+const cors = require("cors");
 
-app.get("/get-contacts", (req, res) => {
+app.get("/get-contacts", cors(), (req, res) => {
     res.send(`${execute("termux-contact-list")}`);
 });
 
 app.get("/get-messages", (req, res) => {
     res.send(`${execute("termux-sms-list -d -n -t all")}`);
 });
+
+app.post("/send-message", cors(), (req, res) => {});
 
 app.listen(8000);
 console.log("Now listening on port 8000");
