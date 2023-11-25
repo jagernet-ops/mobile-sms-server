b0f800afc916229c1d6e1437e5d3715dab90999c
Michael J. Shalewski <mshalewski@outlook.com>
2023-11-25 13:51:21 -0600

    swapped back to lodash for test

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ app.get("/get-messages", (req, res) => {
 
 app.post("/send-single-message", (req, res) => {
     console.log(
-        `termux-sms-send -n ${req.body["phoneNumber"]} ${req.body["messageVal"]}`
+        `termux-sms-send -n ${get(req.body, "phoneNumber")} ${get(
+            req.body,
+            "messageVal"
+        )}`
     );
     res.status(200).end();
 });
