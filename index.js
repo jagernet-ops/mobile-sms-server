72b034b06623652c37672b33226f874f3cc96c8d
Michael J. Shalewski <mshalewski@outlook.com>
2023-11-25 13:36:13 -0600

    added network card parsing and IP address information as well as single message post enpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const execute = require("child_process").execSync;
 const app = express();
+const os = require("os");
 
 const corsMiddleware = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -14,7 +15,7 @@ const corsMiddleware = (req, res, next) => {
     );
     next();
 };
-
+app.use(express.json());
 app.use(corsMiddleware);
 
 app.get("/get-contacts", (req, res) => {
@@ -25,5 +26,26 @@ app.get("/get-messages", (req, res) => {
     res.send(`${execute("termux-sms-list -d -n -t all")}`);
 });
 
+app.post("/send-single-message", (req) => {
+    execute(
+        `termux-sms-send -n ${get(req.body, "phoneNumber")} ${get(
+            req.body,
+            "messageVal"
+        )}`
+    );
+});
+
+//Make group message and multiple number message
+
 app.listen(8000);
-console.log("Now listening on port 8000");
+
+const parseNetworkInterface = (body) => {
+    let ipV4Address = "";
+    ipV4Address = body["Wi-Fi"].filter(
+        (interface) => interface["family"] === "IPv4"
+    )[0]["address"];
+    return ipV4Address;
+};
+console.log(
+    `Now listening at ${parseNetworkInterface(os.networkInterfaces())}:8000`
+);
